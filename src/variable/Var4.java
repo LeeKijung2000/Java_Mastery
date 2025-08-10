ac7791fd55c034098d7bcb8dc82719721d91d8e4
이기정 <163862000+LeeKijung2000@users.noreply.github.com>
2025-08-10 20:21:03 +0900

    변수 선언과 초기화

---
+++ src/variable/Var4.java
@@ -0,0 +1,10 @@
+package variable;
+
+// 변수 선언과 초기화
+public class Var4 {
+    public static void main(String[] args) {
+        int a;
+        int b;
+
+    }
+}
---
+++ src/variable/Var6.java
@@ -0,0 +1,10 @@
+package variable;
+
+// 변수 선언과 초기화
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+        System.out.println("");
+        //선언만하고 초기화를 안한상태에서 a를 넣어준다면 오류가 뜬다
+    }
+}
